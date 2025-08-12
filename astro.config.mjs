06e90e32b06731dd2dcd6893973770caf2e2e40c
Diksha Jain <202414007@dau.ac.in>
2025-08-12 23:40:31 +0530

    made changes memories and added artwork page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
